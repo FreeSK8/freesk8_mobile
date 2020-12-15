91bb3652960456c5776ecd99e2bca38b8a539983
r3n33 <renee1382@gmail.com>
2020-12-15 11:31:33 -0600

    Parse ESC DELTA messages

--- lib/logFileParser.dart
+++ lib/logFileParser.dart
@@ -134,6 +134,70 @@ class LogFileParser {
             }
             break;
           case LOG_MSG_TYPES.ESC_DELTA:
+            print("Parsing ESC DELTA LOG entry");
+            int deltaDT = bytes[i++];
+            ++i; //NOTE: alignment
+            int escID = buffer_get_uint16(bytes, i, Endian.little); i+=2;
+            double deltaVin = buffer_get_int8(bytes, i++) / 10.0;
+            i+=1; //NOTE: alignment
+            double deltaMotorTemp = buffer_get_int8(bytes, i++) / 10.0;
+            double deltaESCTemp = buffer_get_int8(bytes, i++) / 10.0;
+            double deltaDuty = buffer_get_int16(bytes, i) / 10.0; i+=2;
+            double deltaMotorCurrent = buffer_get_int16(bytes, i) / 10.0; i+=2;
+            double deltaBatteryCurrent = buffer_get_int16(bytes, i) / 10.0; i+=2;
+            double deltaWattHours = buffer_get_int8(bytes, i++) / 10.0;
+
+            double deltaWattHoursRegen = buffer_get_int8(bytes, i++) / 10.0;
+            int deltaERPM = buffer_get_int16(bytes, i); i+=2;
+            int deltaEDistance = buffer_get_int16(bytes, i); i+=2;
+            int faultCode = bytes[i++];
+            i+=1; //NOTE: alignment
+            print("ESC DELTA dt $deltaDT id $escID vin $deltaVin mt $deltaMotorTemp et $deltaESCTemp duty $deltaDuty mc $deltaMotorCurrent bc $deltaBatteryCurrent wh $deltaWattHours whr $deltaWattHoursRegen erpm $deltaERPM edist $deltaEDistance f $faultCode");
+
+            if (bytes[i] == PacketEnd) {
+              // Update ESC packet with delta values
+              lastESCPacket.dt = lastESCPacket.dt.add(Duration(seconds: deltaDT));
+              lastESCPacket.escID = escID;
+              lastESCPacket.vIn = doublePrecision(lastESCPacket.vIn + deltaVin, 1);
+              lastESCPacket.motorTemp = doublePrecision(lastESCPacket.motorTemp + deltaMotorTemp, 1);
+              lastESCPacket.mosfetTemp = doublePrecision(lastESCPacket.mosfetTemp + deltaESCTemp,1);
+              lastESCPacket.dutyCycle += deltaDuty;
+              lastESCPacket.motorCurrent += deltaMotorCurrent;
+              lastESCPacket.batteryCurrent += deltaBatteryCurrent;
+              lastESCPacket.wattHours += deltaWattHours;
+              lastESCPacket.wattHoursRegen += deltaWattHoursRegen;
+              lastESCPacket.eRPM += deltaERPM;
+              lastESCPacket.eDistance += deltaEDistance;
+              lastESCPacket.faultCode = faultCode;
+
+              //TODO: duplicated code
+              // Write ESC CSV data
+              convertedFile.writeAsStringSync("${lastESCPacket.dt.toIso8601String().substring(0,19)},"
+                  "esc,"
+                  "${lastESCPacket.escID},"
+                  "${lastESCPacket.vIn},"
+                  "${lastESCPacket.motorTemp},"
+                  "${lastESCPacket.mosfetTemp},"
+                  "${lastESCPacket.dutyCycle},"
+                  "${lastESCPacket.motorCurrent},"
+                  "${lastESCPacket.batteryCurrent},"
+                  "${lastESCPacket.wattHours},"
+                  "${lastESCPacket.wattHoursRegen},"
+                  "${lastESCPacket.eRPM},"
+                  "${lastESCPacket.eDistance},"
+                  "${lastESCPacket.faultCode}\n", mode: FileMode.append);
+
+              // Write faults on their own line
+              if (lastESCPacket.faultCode != 0) {
+                convertedFile.writeAsStringSync("${lastESCPacket.dt.toIso8601String().substring(0,19)},"
+                    "err,"
+                    "${mc_fault_code.values[lastESCPacket.faultCode].toString().substring(14)},"
+                    "${lastESCPacket.faultCode},"
+                    "${lastESCPacket.escID}\n", mode: FileMode.append);
+              }
+            } else {
+              print("logFileParser::parseFile: Unexpected byte at end of packet: bytes[$i] = ${bytes[i]}");
+            }
             break;
           case LOG_MSG_TYPES.GPS:
             print("Parsing GPS LOG entry");
@@ -168,7 +232,7 @@ class LogFileParser {
             double deltaLongitude = buffer_get_int16(bytes, i, Endian.little) / 100000.0; i+=2;
             print("GPS DELTA Time $deltaDt Satellites $deltaSatellites Altitude $deltaAltitude Speed $deltaSpeed Latitude $deltaLatitude Longitude $deltaLongitude");
             if (bytes[i] == PacketEnd) {
-              lastGPSPacket.dt.add(Duration(seconds: deltaDt));
+              lastGPSPacket.dt = lastGPSPacket.dt.add(Duration(seconds: deltaDt));
               lastGPSPacket.satellites += deltaSatellites;
               lastGPSPacket.altitude = doublePrecision(lastGPSPacket.altitude + deltaAltitude, 1);
               lastGPSPacket.speed = doublePrecision(lastGPSPacket.speed - deltaSpeed, 1);
