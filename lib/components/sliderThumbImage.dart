619ff522a716f6e35aa3a4c2568788911c2603bf
r3n33 <renee1382@gmail.com>
2020-12-29 14:11:40 -0600

    The custom slider thumb renderer

---
+++ lib/components/sliderThumbImage.dart
@@ -0,0 +1,42 @@
+
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+
+class SliderThumbImage extends SliderComponentShape {
+  final ui.Image image;
+
+  SliderThumbImage(this.image);
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size(0, 0);
+  }
+
+  @override
+  void paint(PaintingContext context, Offset center,
+      {Animation<double> activationAnimation,
+        Animation<double> enableAnimation,
+        bool isDiscrete,
+        TextPainter labelPainter,
+        RenderBox parentBox,
+        SliderThemeData sliderTheme,
+        TextDirection textDirection,
+        double value,
+        double textScaleFactor,
+        Size sizeWithOverflow}) {
+    final canvas = context.canvas;
+    final imageWidth = image?.width ?? 10;
+    final imageHeight = image?.height ?? 10;
+
+    Offset imageOffset = Offset(
+      center.dx - (imageWidth / 2),
+      center.dy - (imageHeight / 2),
+    );
+
+    Paint paint = Paint()..filterQuality = FilterQuality.high;
+
+    if (image != null) {
+      canvas.drawImage(image, imageOffset, paint);
+    }
+  }
+}
