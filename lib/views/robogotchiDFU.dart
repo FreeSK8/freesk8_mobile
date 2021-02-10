5fa8aa2f1edd21fb894d766c7de53dd7aad95d7f
r3n33 <renee1382@gmail.com>
2021-02-09 21:33:10 -0600

    Include Robogotchi Firmware 0.7.3

--- lib/views/robogotchiDFU.dart
+++ lib/views/robogotchiDFU.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_nordic_dfu/flutter_nordic_dfu.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:freesk8_mobile/globalUtilities.dart';
 
-const String updateFileName = "Robogotchi_0.7.2"; //TODO: NOTE: Must match that of /assets/firmware/<*>.zip
+const String updateFileName = "Robogotchi_0.7.3"; //TODO: NOTE: Must match that of /assets/firmware/<*>.zip
 
 class RobogotchiDFU extends StatefulWidget {
   @override
