2a35555900bbe364b3ace9dba37dabdaaa2757a2
r3n33 <renee1382@gmail.com>
2021-05-14 17:03:59 -0500

    Revert changes from autoStopHandler

--- lib/components/autoStopHandler.dart
+++ lib/components/autoStopHandler.dart
@@ -5,16 +5,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AutoStopHandler extends WidgetsBindingObserver {
-  static bool _unexpectedDisconnect;
-  static ValueChanged<int> _delayedTabControllerIndexChange;
-
-  @override
-  AutoStopHandler(ValueChanged<int> tabChangeFunc, bool unexpectedDisconnect)
-  {
-    _delayedTabControllerIndexChange = tabChangeFunc;
-    _unexpectedDisconnect = unexpectedDisconnect;
-  }
-
   @override
   Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
     switch (state) {
@@ -29,12 +19,6 @@ class AutoStopHandler extends WidgetsBindingObserver {
         break;
       case AppLifecycleState.resumed:
         globalLogger.d("AppLifecycleState: Resumed");
-        // If the application has disconnected in the background we
-        // need to navigate back to the connection tab upon resuming
-        if (_unexpectedDisconnect) {
-          globalLogger.d("AppLifecycleState: Navigating to Connection tab (unexpectedDisconnect)");
-          _delayedTabControllerIndexChange(controllerViewConnection);
-        }
         break;
     }
   }
